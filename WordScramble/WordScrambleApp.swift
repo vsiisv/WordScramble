c80bd1faee2ae0888de359cc30039e424d758301
vsiisv <shores.freezer0u@icloud.com>
2024-09-17 07:13:54 +1000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by VASILY IKONNIKOV on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
